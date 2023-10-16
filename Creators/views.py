46059c94771b48d00894edad9e47ca201964b28a
01010101010102 <ruanramalho1234@gmail.com>
2023-10-16 15:02:49 -0300

    testando endpoints

--- Creators/views.py
+++ Creators/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+from django.http import FileResponse
+import os
 
+caminho_absoluto = 'C:\\Users\\IFMA\\GIT\\Django-React\\templates\\CreatorsStatic\\meia_talha.jpg'
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # Abra o arquivo de imagem em modo de leitura binária
+    with open(caminho_absoluto, 'rb') as image_file:
+        # Crie uma resposta de arquivo para a imagem
+        response = FileResponse(image_file)
+
+    return response
